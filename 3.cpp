01861a1d008e557e074e6c690621940f1948aed2
Nishit <nishitdxt@gmail.com>
2021-06-21 14:55:06 +0530

    day4

--- 3.cpp
+++ 3.cpp
@@ -1,33 +1,32 @@
 //Initial function template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int kthSmallest(int *, int, int, int);
- 
+
 int main()
 {
     // ios_base::sync_with_stdio(false);
     // cin.tie(NULL);
-    
+
     int test_case;
-    cin>>test_case;
-    while(test_case--)
+    cin >> test_case;
+    while (test_case--)
     {
         int number_of_elements;
-        cin>>number_of_elements;
+        cin >> number_of_elements;
         int a[number_of_elements];
-        
-        for(int i=0;i<number_of_elements;i++)
-            cin>>a[i];
-            
+
+        for (int i = 0; i < number_of_elements; i++)
+            cin >> a[i];
+
         int k;
-        cin>>k;
-        cout<<kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+        cin >> k;
+        cout << kthSmallest(a, 0, number_of_elements - 1, k) << endl;
     }
     return 0;
-}// } Driver Code Ends
-
+} // } Driver Code Ends
 
 //User function template for C++
 
@@ -35,12 +34,10 @@ int main()
 // l : starting index of the array i.e 0
 // r : ending index of the array i.e size-1
 // k : find kth smallest element and return using this function
-int kthSmallest(int arr[], int l, int r, int k) {
-    
-    
-    sort(arr+l,arr+r+1);
-    
-    
-    
-    return arr[l+k-1];
+int kthSmallest(int arr[], int l, int r, int k)
+{
+
+    sort(arr + l, arr + r + 1);
+
+    return arr[l + k - 1];
 }
