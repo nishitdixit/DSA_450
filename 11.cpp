d48f634e2ffde46ec8aa973f8d08e25f20ad0d9d
Nishit <nishitdxt@gmail.com>
2021-06-24 23:58:02 +0530

    day5

--- 11.cpp
+++ 11.cpp
@@ -11,20 +11,44 @@ class Solution
 public:
     int findDuplicate(vector<int> &nums)
     {
-        int expected_xor = 0;
-        int n = nums.size() - 1;
-        for (int i = 1; i <= n; i++)
+        if (nums.size() <= 1)
+            return -1;
+        int p1 = nums[0];
+        int p2 = nums[nums[0]];
+        while (p1 != p2)
         {
-            expected_xor = expected_xor ^ i;
+            p1 = nums[p1];
+            p2 = nums[nums[p2]];
         }
-        int xor_arr = 0;
-        for (int i = 0; i <= n; i++)
+        p2 = 0;
+        while (p1 != p2)
         {
-            xor_arr = xor_arr ^ nums[i];
+            p1 = nums[p1];
+            p2 = nums[p2];
         }
-        return (xor_arr ^ expected_xor);
+        return p1;
     }
 };
+
+// class Solution
+// {
+// public:
+//     int findDuplicate(vector<int> &nums)
+//     {
+//         int expected_xor = 0;
+//         int n = nums.size() - 1;
+//         for (int i = 1; i <= n; i++)
+//         {
+//             expected_xor = expected_xor ^ i;
+//         }
+//         int xor_arr = 0;
+//         for (int i = 0; i <= n; i++)
+//         {
+//             xor_arr = xor_arr ^ nums[i];
+//         }
+//         return (xor_arr ^ expected_xor);
+//     }
+// };
 
 // { Driver Code Starts.
 
@@ -45,7 +69,6 @@ int main()
             // cout << i << " ";
             nums.push_back(arr[i]);
         }
-        
     }
     return 0;
 }
